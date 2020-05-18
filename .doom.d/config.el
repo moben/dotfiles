b928c83e01eb55af824cb1e6f3209678d129a43f
Benedikt Morbach <benedikt.morbach@googlemail.com>
2020-05-18 09:15:42 +0200

    Doom: include f.el to check system state

--- .doom.d/config.el
+++ .doom.d/config.el
@@ -3,6 +3,7 @@
 ;; Place your private configuration here! Remember, you do not need to run 'doom
 ;; sync' after modifying this file!
 
+(use-package! f)
 
 ;; Some functionality uses this to identify you, e.g. GPG configuration, email
 ;; clients, file templates and snippets.
